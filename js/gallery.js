2c7d06f0a06fd392c34e749b0597489c2c8de10f
zajovic <zajovic@hotmail.com>
2019-06-13 17:23:31 +0200

    *

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,7 @@ function openModal() {
 }
 
 function closeModal() {
+    removeAlt();
     document.getElementById("slide-modal").style.display = "none";
 }
 
@@ -18,9 +19,27 @@ var slideIndex = 1;
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
+function removeAlt() {
+    let divAlt = document.getElementById("caption");
+    
+    if (divAlt !== null) {
+        divAlt.remove();
+    }
+}
+
+function displayAlt() {
+    let slides = document.getElementsByClassName("zoomImg");
+    let slidesAlt = slides[slideIndex - 1].alt;
+    
+    let caption = document.createElement("div");
+    caption.id = "caption";
+    caption.innerHTML = slidesAlt;
+    let modal = document.getElementById("slide-modal");
+    modal.appendChild(caption);
+}
 
 function showSlides(n) {
-    var slides = document.getElementsByClassName("zoomImg");
+    let slides = document.getElementsByClassName("zoomImg");
     if (n > slides.length) {
         slideIndex = 1
     };
@@ -32,8 +51,11 @@ function showSlides(n) {
     }
 
     slides[slideIndex - 1].style.display = "block";
+    
+    displayAlt();
 }
 
 function plusSlides(n) {
+    removeAlt();
     showSlides(slideIndex += n);
 }
